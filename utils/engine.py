8ff184b2d63f43c6b6e9289a338f111802f7afd7
Daming-W <83384577+Daming-W@users.noreply.github.com>
2023-06-19 23:20:02 +0800

    Update engine.py

--- utils/engine.py
+++ utils/engine.py
@@ -4,10 +4,11 @@ import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 from tqdm import tqdm
+from .metrics import concordance_correlation_coefficient
 
-def train_one_epoch(dataloader, model, optimizer, criterion):
-    # set loss list
-    total_loss = []
+def train_one_epoch(args, dataloader, model, optimizer, criterion):
+    # set loss and acc lists
+    total_loss,total_acc = [],[]
     # train mode
     model.train()
     # loop batch
@@ -16,23 +17,31 @@ def train_one_epoch(dataloader, model, optimizer, criterion):
             # clear optimizer
             optimizer.zero_grad()
             # model computation
-            outputs = model(inputs)
-            print(outputs)
-            print(targets)
+            if args.method == 'baseline': 
+                outputs = model(inputs)
+            elif args.method == 'TFN':
+                outputs = model(inputs[0],inputs[1],inputs[2])
+            else:
+                NotImplementedError
+            # loss and acc
             loss = criterion(outputs, targets)
+            acc = concordance_correlation_coefficient(outputs.detach().numpy(), targets.detach().numpy())
             # backward and step over
             loss.backward()
             optimizer.step()
             # sum losses in an epoch
             total_loss.append(loss.detach().cpu())
+            total_acc.append(acc)
             pbar.set_description('training')
             pbar.set_postfix({'loss(iter)':float(loss.detach().cpu()), 
-                              'loss(mean)':np.mean(total_loss)})
+                              'loss(mean)':np.mean(total_loss),
+                              'acc(iter)':acc,
+                              'acc(mean)':np.mean(total_acc)})
             pbar.update(1)
 
-def evaluate_one_epoch(dataloader, model, criterion):
-    # set loss list
-    total_loss = []
+def evaluate_one_epoch(args, dataloader, model, criterion):
+    # set loss and acc lists
+    total_loss,total_acc = [],[]
     # train mode
     model.eval()
     # loop batch
@@ -40,12 +49,24 @@ def evaluate_one_epoch(dataloader, model, criterion):
         for i, (inputs, targets) in enumerate(dataloader):
             # model computation
             with torch.no_grad():
-                outputs = model(inputs)
-                loss = criterion(outputs, targets)
+                # model computation
+                if args.method == 'baseline': 
+                    outputs = model(inputs)
+                elif args.method == 'TFN':
+                    outputs = model(inputs[0],inputs[1],inputs[2])
+                else:
+                    NotImplementedError
+            # loss and acc
+            loss = criterion(outputs, targets)
+            acc = concordance_correlation_coefficient(outputs.detach().numpy(), targets.detach().numpy())
             # sum losses in a epoch
             total_loss.append(loss.detach().cpu())
+            total_acc.append(acc)
             pbar.set_description('evaluation')
-            pbar.set_postfix({'loss(iter)':float(loss.detach().cpu()), 'loss(mean)':np.mean(total_loss)})
+            pbar.set_postfix({'loss(iter)':float(loss.detach().cpu()), 
+                              'loss(mean)':np.mean(total_loss),
+                              'acc(iter)':acc,
+                              'acc(mean)':np.mean(total_acc)})
             pbar.update(1)
 
         
