710efad70982fdaeb86d3af054e63cdff502c6c7
Daming-W <83384577+Daming-W@users.noreply.github.com>
2023-06-19 23:20:16 +0800

    Update model.py

--- utils/model.py
+++ utils/model.py
@@ -11,13 +11,15 @@ from torch.nn.parameter import Parameter
 #################
 # call function #
 #################
-def get_model(flag, input_shapes, hidden_units, output_units):
-    if flag == 'simple':
-        model = ModalityFusionModel_simple(input_shapes, hidden_units, output_units)
-    elif flag == 'attention':
-        model = ModalityFusionModule_attention(input_shapes, hidden_units, output_units)
+def get_model(args):
+
+    if args.method == 'baseline':
+        model = ModalityFusionModel_simple(args.baseline_dim_list[0], args.baseline_dim_list[1], args.baseline_dim_list[2])
+    elif args.method == 'TFN':
+        model = TFN(args.TFN_hidden_dims, args.TFN_dropouts, args.TFN_post_fusion_dim)
     else:
         NotImplementedError
+
     return model
 
 ############################
@@ -105,52 +107,33 @@ modified for ACM-MM 2023
 '''
 class TFN(nn.Module):
 
-    def __init__(self, input_dims, hidden_dims, dropouts, post_fusion_dim):
-        '''
-        Args:
-            input_dims - a length-3 tuple, contains (audio_dim, video_dim, text_dim)
-            hidden_dims - another length-3 tuple, similar to input_dims
-            #text_out - int, specifying the resulting dimensions of the text subnetwork
-            dropouts - a length-4 tuple, contains (audio_dropout, video_dropout, text_dropout, post_fusion_dropout)
-            post_fusion_dim - int, specifying the size of the sub-networks after tensorfusion
-        Output:
-            (return value in forward) a scalar value between -3 and 3
-        '''
+    def __init__(self, hidden_dims, dropouts, post_fusion_dim):
         super(TFN, self).__init__()
 
-        # dimensions are specified in the order of audio, video and text
         self.audio_hidden = hidden_dims[0]
         self.video_hidden = hidden_dims[1]
-        self.text_hidden = hidden_dims[2]
-        #self.text_out= text_out
+        self.kinect_hidden = hidden_dims[2]
         self.post_fusion_dim = post_fusion_dim
 
         self.audio_prob = dropouts[0]
         self.video_prob = dropouts[1]
-        self.text_prob = dropouts[2]
+        self.kinect_prob = dropouts[2]
         self.post_fusion_prob = dropouts[3]
 
-        # define the post_fusion layers
         self.post_fusion_dropout = nn.Dropout(p=self.post_fusion_prob)
-        self.post_fusion_layer_1 = nn.Linear((self.text_hidden + 1) * (self.video_hidden + 1) * (self.audio_hidden + 1), self.post_fusion_dim)
+        self.post_fusion_layer_1 = nn.Linear((self.audio_hidden + 1) * (self.video_hidden + 1) * (self.kinect_hidden + 1), self.post_fusion_dim)
         self.post_fusion_layer_2 = nn.Linear(self.post_fusion_dim, self.post_fusion_dim)
         self.post_fusion_layer_3 = nn.Linear(self.post_fusion_dim, 1)
 
-        # in TFN we are doing a regression with constrained output range: (-3, 3), hence we'll apply sigmoid to output
-        # shrink it to (0, 1), and scale\shift it back to range (-3, 3)
         self.output_range = Parameter(torch.FloatTensor([6]), requires_grad=False)
         self.output_shift = Parameter(torch.FloatTensor([-3]), requires_grad=False)
 
-    def forward(self, audio_h, video_h, text_h):
-        '''
-        Args:
-            audio_x: tensor of shape (batch_size, audio_in)
-            video_x: tensor of shape (batch_size, video_in)
-            text_x: tensor of shape (batch_size, sequence_len, text_in)
-        '''
+
+    def forward(self, audio_h, video_h, kinect_h):
+
+
         batch_size = audio_h.data.shape[0]
 
-        # next we perform "tensor fusion", which is essentially appending 1s to the tensors and take Kronecker product
         if audio_h.is_cuda:
             DTYPE = torch.cuda.FloatTensor
         else:
@@ -158,8 +141,8 @@ class TFN(nn.Module):
 
         _audio_h = torch.cat((Variable(torch.ones(batch_size, 1).type(DTYPE), requires_grad=False), audio_h), dim=1)
         _video_h = torch.cat((Variable(torch.ones(batch_size, 1).type(DTYPE), requires_grad=False), video_h), dim=1)
-        _text_h = torch.cat((Variable(torch.ones(batch_size, 1).type(DTYPE), requires_grad=False), text_h), dim=1)
-
+        _kinect_h = torch.cat((Variable(torch.ones(batch_size, 1).type(DTYPE), requires_grad=False), kinect_h), dim=1)
+    
         # _audio_h has shape (batch_size, audio_in + 1), _video_h has shape (batch_size, _video_in + 1)
         # we want to perform outer product between the two batch, hence we unsqueenze them to get
         # (batch_size, audio_in + 1, 1) X (batch_size, 1, video_in + 1)
@@ -170,7 +153,7 @@ class TFN(nn.Module):
         # we have to reshape the fusion tensor during the computation
         # in the end we don't keep the 3-D tensor, instead we flatten it
         fusion_tensor = fusion_tensor.view(-1, (self.audio_hidden + 1) * (self.video_hidden + 1), 1)
-        fusion_tensor = torch.bmm(fusion_tensor, _text_h.unsqueeze(1)).view(batch_size, -1)
+        fusion_tensor = torch.bmm(fusion_tensor, _kinect_h.unsqueeze(1)).view(batch_size, -1)
 
         post_fusion_dropped = self.post_fusion_dropout(fusion_tensor)
         post_fusion_y_1 = F.relu(self.post_fusion_layer_1(post_fusion_dropped))
@@ -178,7 +161,7 @@ class TFN(nn.Module):
         post_fusion_y_3 = F.sigmoid(self.post_fusion_layer_3(post_fusion_y_2))
         output = post_fusion_y_3 * self.output_range + self.output_shift
 
-        return output
+        return output.view(-1)
 
 if __name__ == '__main__':
     # Example usage
@@ -189,12 +172,11 @@ if __name__ == '__main__':
     # Get model
     #model = ModalityFusionModule_attention(input_shapes, hidden_units, output_units)
     model = TFN(
-        input_dims=[None,None,None],
         hidden_dims=[1023,314,367], #video,audio,kinect
         dropouts=[0.01,0.01,0.01,0.01],
         post_fusion_dim=256
     )
-
+    print('loading model finish')
     # Example forward pass
     input_data = torch.randn(161798, 83)  # Your concatenated input data
 
